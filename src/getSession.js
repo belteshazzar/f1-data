46cd71dbf37abd8663eda563c4560e335ef91f76
Daniel Walton <daniel@belteshazzar.com.au>
2025-03-29 16:23:12 +1030

    q results

--- src/getSession.js
+++ src/getSession.js
@@ -2,15 +2,22 @@ import fs from 'fs';
 import yaml from 'js-yaml';
 
 export function getSession(values) {
-  let session = values.session || 'race';
-  if (session != 'race' && session != 'qualifying' && session != 'sprint') {
-    throw new Error(`Invalid session: ${session}`);
-  }
+  let session = values.session || 'r';
 
-  const url = `https://api.jolpi.ca/ergast/f1/${values.year}/${values.round}/${session == 'race' ? 'results' : session}/?limit=100&format=json`
+  if (session == 'r') {
+    session = 'results';
+  } else if (session == 'q') {
+    session = 'qualifying';
+  } else if (session == 's') {
+    session = 'sprint';
+  } else {
+    throw new Error(`Invalid session: ${session}`);
+  } 
+
+  const url = `https://api.jolpi.ca/ergast/f1/${values.year}/${values.round}/${session}/?limit=100&format=json`
   const filename = `ergast/${values.year}-${values.round}-${session}.yaml`
 
-  console.log(`Getting ${session} for round ${values.round} of ${values.year}`);
+  console.log(`\nGetting ${session} for round ${values.round} of ${values.year}`);
   console.log(`- url: ${url}`);
   console.log(`- file: ${filename}`);
 
