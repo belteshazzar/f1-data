3f5b0062531537145d1db1c5f0135e8fda0388b9
Daniel Walton <daniel@belteshazzar.com.au>
2025-04-04 22:10:58 +1030

    25 japan friday

--- src/getSession.js
+++ src/getSession.js
@@ -11,7 +11,7 @@ export function getSession(values) {
     session = 'qualifying';
   } else if (session == 's') {
     session = 'sprint';
-  } else if (session == 'sq') {
+  } else if (session == 'p1' || session == 'p2' || session == 'p3' || session == 'sq') {
     getSessionFromF1(values);
     return
   } else {
@@ -36,8 +36,10 @@ export function getSession(values) {
 
 function getSessionFromF1(values) {
 
-  if (values.session == 'sq') {
-    getSessionFromF1SQ(values)
+  if (values.session == 'p1' || values.session == 'p2' || values.session == 'p3') {
+    getSessionFromF1Practice(values)
+  } else if (values.session == 'sq') {
+      getSessionFromF1SQ(values)
   } else {
     throw new Error(`invalid session ${values.session}`)
   }
@@ -88,6 +90,71 @@ function getSessionFromF1SQ(values) {
         result.q1 = $(tds[4]).text()
         result.q2 = $(tds[5]).text()
         result.q3 = $(tds[6]).text()
+
+        data.results.push(result);
+      });
+
+      fs.writeFileSync(filename, yaml.dump(data));
+    });
+}
+
+function getSessionFromF1Practice(values) {
+
+  const rounds = yaml.load(fs.readFileSync(`data/${values.year}-rounds.yaml`, 'utf8'));
+  const drivers = yaml.load(fs.readFileSync(`data/${values.year}-drivers.yaml`, 'utf8'));
+  const constructors = yaml.load(fs.readFileSync(`data/${values.year}-constructors.yaml`, 'utf8'));
+  const race = rounds.rounds[values.round-1]
+  const practice = values.session.substr(1)
+
+  const raceNum = race.raceNum + 94 // F1 Race Number Offset
+  const country = race.circuit.location.country.toLowerCase()
+  const url = `https://www.formula1.com/en/results/${values.year}/races/${raceNum}/${country}/practice/${practice}`
+  const filename = `data/${values.year}-${values.round}-practice-${practice}.yaml`
+
+  console.log(`\nGetting practice ${practice} for round ${values.round} of ${values.year}`);
+  console.log(`- url: ${url}`)
+  console.log(`- file: ${filename}`)
+
+  fetch(url)
+    .then(res => res.text())
+    .then(text => {
+      const $ = cheerio.load(text);
+
+      const title = $('title').text().toLowerCase()
+      console.log(`\n- page title: ${title}`)
+
+      const data = {
+        season: values.year,
+        round: values.round,
+        session: 'practice ' + practice,
+        results: [],
+      }
+
+      $('table.f1-table > tbody > tr').each(function() {
+        const result = {}
+
+        const tds = $(this).find('td')
+        result.position = $(tds[0]).text()*1
+
+        const driverCode = $(tds[2]).find('span:nth(2)').text()
+        const driver = drivers.drivers.find(d => d.code == driverCode)
+        if (!driver) {
+          throw new Error(`driver not found: ${driverCode}`)
+        }
+        result.driverId = driver.driverId
+
+        const constructorName = $(tds[3]).text()
+        const constructor = constructors.constructors.find(d => {
+          return d.knownAs.find(knownAs => knownAs == constructorName)
+        })
+        if (!constructor) {
+          throw new Error(`constructor not found: ${constructorName}`)
+        }
+        result.constructorId = constructor.constructorId
+
+        result.time = $(tds[4]).text()
+        result.gap = $(tds[5]).text()
+        result.laps = $(tds[6]).text()
 
         data.results.push(result);
       });
